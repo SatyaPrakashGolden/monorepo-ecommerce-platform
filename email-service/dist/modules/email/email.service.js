08d2229599d09eb37d4434006de01adde694ea35
root <root@fujitsu.>
2025-09-29 01:53:01 +0000

    Add notification-service into monorepo

---
+++ email-service/dist/modules/email/email.service.js
@@ -0,0 +1,59 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.EmailService = void 0;
+const common_1 = require("@nestjs/common");
+const nodemailer = require("nodemailer");
+const dotenv = require("dotenv");
+const microservices_1 = require("@nestjs/microservices");
+dotenv.config();
+let EmailService = class EmailService {
+    kafkaClient;
+    transporter;
+    constructor(kafkaClient) {
+        this.kafkaClient = kafkaClient;
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST || 'smtp-relay.brevo.com',
+            port: Number(process.env.SMTP_PORT) || 587,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS,
+            },
+        });
+    }
+    async sendMail(to, subject, html) {
+        try {
+            await this.transporter.sendMail({
+                from: `"Carbike360 EV Bikes" <${process.env.FROM_EMAIL}>`,
+                to,
+                subject,
+                html,
+            });
+            console.log(`✅ Email sent to ${to}`);
+        }
+        catch (error) {
+            console.error('❌ Error sending email:', error);
+            throw error;
+        }
+    }
+};
+exports.EmailService = EmailService;
+exports.EmailService = EmailService = __decorate([
+    (0, common_1.Injectable)(),
+    __param(0, (0, common_1.Inject)('KAFKA_SERVICE')),
+    __metadata("design:paramtypes", [typeof (_a = typeof microservices_1.ClientKafka !== "undefined" && microservices_1.ClientKafka) === "function" ? _a : Object])
+], EmailService);
+//# sourceMappingURL=email.service.js.map
